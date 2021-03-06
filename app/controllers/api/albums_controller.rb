3f5ec90d3c9bb0490f63240ef61167801f56cc2a
Thom Konan <thomkonan@gmail.com>
2021-03-06 15:14:26 -0500

    Controller updated

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -1,17 +1,36 @@
 class Api::AlbumsController < ApplicationController
   def index
-    render
+    @albums = Album.all
+    render "index.json.jb"
   end
 
   def create
-  end
-
-  def update
+    @album = Album.new(
+      title: params[:title],
+      artist: params[:artist],
+      year: params[:year],
+    )
+    @album.save
+    render "show.json.jb"
   end
 
   def show
+    @album = Album.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @album = Album.find_by(id: params[:id])
+    @album.title = params[:title] || album.title
+    @album.artist = params[:artist] || album.artist
+    @album.year = params[:year] || album.year
+    @album.save
+    render "show.json.jb"
   end
 
   def destroy
+    @album = Album.find_by(id: params[:id])
+    @album.destroy
+    render json: {message: "Album has been successfully destroyed."}
   end
 end
